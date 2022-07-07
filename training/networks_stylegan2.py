699a159e9912cca426818164767362d697c76009
weient <weien@gapp.nthu.edu.tw>
2022-07-07 19:54:17 +0800

    add print x

--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -493,7 +493,9 @@ def G_synthesis_stylegan2(
     with tf.variable_scope('4x4'):
         with tf.variable_scope('Const'):
             x = tf.get_variable('const', shape=[1, nf(1), 4, 4], initializer=tf.initializers.random_normal())
+            print(x)
             x = tf.tile(tf.cast(x, dtype), [tf.shape(dlatents_in)[0], 1, 1, 1])
+            print(x)
         with tf.variable_scope('Conv'):
             x = layer(x, layer_idx=0, fmaps=nf(1), kernel=3)
         if architecture == 'skip':

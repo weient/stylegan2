b2d709fd07683b91d7cae482d486f59687ba4121
weient <weien@gapp.nthu.edu.tw>
2022-07-11 17:28:09 +0800

    numpy x

--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -496,6 +496,7 @@ def G_synthesis_stylegan2(
             #print(x)
             x = tf.tile(tf.cast(x, dtype), [tf.shape(dlatents_in)[0], 1, 1, 1])
             #print("dlatents_in shape: ", tf.shape(dlatents_in)[0])
+            print("numpy x: ", x.numpy())
             print("x (generator的input) shape: ", tf.shape(x))
         with tf.variable_scope('Conv'):
             x = layer(x, layer_idx=0, fmaps=nf(1), kernel=3)

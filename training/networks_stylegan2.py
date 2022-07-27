bf661b874e1bd5e655c8c4b0e91d3b3fb791d143
weient <weien@gapp.nthu.edu.tw>
2022-07-27 18:48:37 +0800

    mod

--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -497,17 +497,17 @@ def G_synthesis_stylegan2(
     y = None
     with tf.variable_scope('4x4'):
         with tf.variable_scope('Const'):
-            x = tf.get_variable('const', shape=[1, nf(1), 4, 4], initializer=tf.initializers.random_normal())
-            x = tf.tile(tf.cast(x, dtype), [tf.shape(dlatents_in)[0], 1, 1, 1])
-            return_val = tf.assign(x, tmp)
-            with tf.Session() as sess:
-                print(sess.run(return_val))
+            #x = tf.get_variable('const', shape=[1, nf(1), 4, 4], initializer=tf.initializers.random_normal())
+            #x = tf.tile(tf.cast(x, dtype), [tf.shape(dlatents_in)[0], 1, 1, 1])
+            #return_val = tf.assign(x, tmp)
+            #with tf.Session() as sess:
+            #    print(sess.run(return_val))
             # x應該就是generator的input（隨機的），要改成content encoder的output
             #x = tf.get_variable('const', shape=[1, nf(1), 4, 4], initializer=tf.initializers.random_normal())
             #print(x)
             #x = tf.tile(tf.cast(x, dtype), [tf.shape(dlatents_in)[0], 1, 1, 1])
             #print("dlatents_in shape: ", tf.shape(dlatents_in)[0])
-            
+            x = tmp
             #print("numpy x: ", x.shape.as_list())
             #print("x (generator的input) shape: ", x)
         with tf.variable_scope('Conv'):
